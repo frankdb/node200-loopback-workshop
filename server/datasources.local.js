4f8ae9351c8d5beac9f243fb47554567853097bf
frankdb <fd.barros@yahoo.com>
2019-05-03 08:49:04 -0700

    Init commit

---
+++ server/datasources.local.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "db": {
+    "name": "db",
+    "connector": "memory"
+  },
+  "MongoDB": {
+    "url": "mongodb://localhost:27017",
+    "name": "MongoDB",
+    "connector": "mongodb"
+  }
+}
